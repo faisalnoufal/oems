a79027983e6b3572822b84c7938eb3052a8b3390
Faisal Athikkai <faisalnoufal@gmail.com>
2021-04-19 05:53:39 +0530

    webadder.min

--- js/splide-2.4.21/webadder.js
+++ js/splide-2.4.21/webadder.js
@@ -1,29 +1,19 @@
 document.body.onload = webadder;
 src = "//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js"
-
-// WebFont.load({
-// 	google: {
-// 		families: ["Raleway:100,400,700,800,900"],
-// 	},
-// });
-
 WebFontConfig = {
 	google: {
 		families: ['Raleway:100,400,700,800,900']
 	}
 };
-
 (function (d) {
 	var wf = d.createElement('script'), s = d.scripts[0];
 	wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
 	wf.async = true;
 	s.parentNode.insertBefore(wf, s);
 })(document);
-
 function webadder() {
 	let atag = document.createElement("a");
 	let link = document.createTextNode("webAdder");
-
 	atag.appendChild(link);
 	atag.href = "https://webadder.com";
 	atag.target = "_blank";
@@ -38,9 +28,7 @@ function webadder() {
 	atag.onmouseout = function () {
 		atag.style.color = "white";
 	};
-}
-
-// Messenger Chat plugin Code
+}// Messenger Chat plugin Code
 
 window.fbAsyncInit = function () {
 	FB.init({
