a46525251515444f02f705020c55b1a6b1deb836
Faisal Athikkai <faisalnoufal@gmail.com>
2021-05-16 20:28:28 +0530

    logo, header, transport update

--- js/splide-2.4.21/webadder.js
+++ js/splide-2.4.21/webadder.js
@@ -1,19 +1,29 @@
 document.body.onload = webadder;
 src = "//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js"
+
+// WebFont.load({
+// 	google: {
+// 		families: ["Raleway:100,400,700,800,900"],
+// 	},
+// });
+
 WebFontConfig = {
 	google: {
 		families: ['Raleway:100,400,700,800,900']
 	}
 };
+
 (function (d) {
 	var wf = d.createElement('script'), s = d.scripts[0];
 	wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
 	wf.async = true;
 	s.parentNode.insertBefore(wf, s);
 })(document);
+
 function webadder() {
 	let atag = document.createElement("a");
 	let link = document.createTextNode("webAdder");
+
 	atag.appendChild(link);
 	atag.href = "https://webadder.com";
 	atag.target = "_blank";
@@ -28,4 +38,39 @@ function webadder() {
 	atag.onmouseout = function () {
 		atag.style.color = "white";
 	};
+}
+
+// Messenger Chat plugin Code
+
+window.fbAsyncInit = function () {
+	FB.init({
+		xfbml: true,
+		version: "v10.0",
+	});
+};
+
+(function (d, s, id) {
+	var js,
+		fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s);
+	js.id = id;
+	js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+	fjs.parentNode.insertBefore(js, fjs);
+})(document, "script", "facebook-jssdk");
+
+function initMap() {
+	// The location of Uluru
+	const oEMSPalad = { lat: 11.3602567, lng: 76.3284 };
+	// The map, centered at Uluru
+	const map = new google.maps.Map(document.getElementById("map"), {
+		zoom: 16,
+		center: oEMSPalad,
+	});
+	// The marker, positioned at Uluru
+	const marker = new google.maps.Marker({
+		position: oEMSPalad,
+		map: map,
+		//icon: "/assets/images/dmd.gif"
+	});
 }
